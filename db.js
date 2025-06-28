7fec1574f4f60dc733d9da37413097672ce8e72d
Sarvesh Chaudhary <sc418760@gmail.com>
2025-06-28 14:17:38 +0530

    changes in database

--- db.js
+++ db.js
@@ -1,14 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import pg from "pg";
 const {Pool} =pg;
 
+// const pool = new Pool({
+//     user: "postgres",
+//     host: "localhost",
+//     database: "code_reviewer",
+//     password: "sarveshdbms",
+//     port: 5432, // Default PostgreSQL port
+// });
+console.log("🔍 DATABASE_URL:", process.env.DATABASE_URL); 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "code_reviewer",
-    password: "sarveshdbms",
-    port: 5432, // Default PostgreSQL port
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
 pool.connect()
     .then(() => console.log("✅ PostgreSQL connected..."))
     .catch((err) => console.error("❌ Database connection failed:", err));
